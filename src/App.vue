337cd40b11a05b1cfe39eedf7282283e1a6cd433
Stefan Simon <stefan.simon@xenadu.de>
2021-08-24 07:31:07 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueWizzard v-show="showDialog" header="Wizzard-State-Manager" :states="states" @finish="finish" @next="next" @back="back" @close="close">
+      <template v-slot:state0>
+        <form>
+          <label>Eingabe Name</label>
+          <input type="text" v-model="user.name">
+        </form>
+      </template>
+      <template v-slot:state1>
+        <form>
+          <label>Eingabe E-Mail</label>
+          <input type="email" v-model="user.email">
+        </form>
+      </template>
+      <template v-slot:state2>
+        <form>
+          <label>Eingabe Alter</label>
+          <input type="number" v-model="user.alter">
+        </form>
+      </template>
+    </VueWizzard>
+    <button class="btn btn-primary" @click="showDialog = !showDialog">Toggle</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueWizzard from "./components/VueWizzard";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueWizzard,
+  }, data() {
+    return {
+      showDialog: false,
+      user: {
+        name: 'Kein Name',
+        alter: 30,
+        email: 'muh@test.de',
+      },
+      states: [
+          'Username',
+          'E-Mail',
+          'Alter'
+      ],
+    };
+  },
+  methods: {
+    close() {
+      this.showDialog = false
+    },
+    finish() {
+      console.log('onFinish triggered');
+    },
+    next(currentState) {
+      console.log('onNext triggered. toState = ', currentState);
+    },
+    back(currentState) {
+      console.log('onBack triggered. toState = ', currentState);
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*width: 50vw;*/
+  /*position: absolute;*/
+  /*left: 25vw;*/
+  /*top: 10vw;*/
 }
 </style>
